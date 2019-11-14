d1eb8cafb825c078cd50388abcb3e94b232de776
Adam Romański <adam.romanski@netguru.pl>
2019-11-14 20:37:42 +0100

    Basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
